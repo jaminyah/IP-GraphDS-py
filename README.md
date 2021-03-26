b46e053282a147f3b3ac69a44219650074328550
jaminyah <jaminyah@gmail.com>
2021-03-26 02:37:31 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+Add depth first search (DFS) traversal code to graph data structure tutorial:
+reference: https://www.bogotobogo.com/python/python_graph_data_structures.php
+
+DFS Implementation based on:
+reference: https://eddmann.com/posts/depth-first-search-and-breadth-first-search-in-python/
---
+++ graph.py
@@ -0,0 +1,78 @@
+from vertex import Vertex
+
+class Graph:
+    def __init__(self):
+        self.vert_dict = {}
+        self.num_vertices = 0
+    
+    def __iter__(self):
+        return iter(self.vert_dict.values())
+    
+    def add_vertex(self, node):
+        self.num_vertices = self.num_vertices + 1
+        new_vertex = Vertex(node)
+        self.vert_dict[node] = new_vertex
+        return new_vertex
+    
+    def get_adjacent(self, n):
+        if n in self.vert_dict:
+            return self.vert_dict[n]
+        else:
+            return None
+    
+    def add_edge(self, frm, to, cost=0):
+        if frm not in self.vert_dict:
+            self.add_vertex(frm)
+        if to not in self.vert_dict:
+            self.add_vertex(to)
+
+        self.vert_dict[frm].add_neighbor(self.vert_dict[to], cost)
+        self.vert_dict[to].add_neighbor(self.vert_dict[frm], cost)
+
+    def get_vertices(self):
+        return self.vert_dict.keys()
+
+    def dfs(self, start):
+        visted, stack = set(), [start]
+        adjacent = set()
+
+        while stack:
+            vertex = stack.pop()
+            if vertex not in visted:
+                print(f"visted add vertex: {vertex}")
+                visted.add(vertex)
+                node = self.vert_dict[vertex]
+                
+                for n in node.get_connections():
+                    adjacent.add(n.get_id())
+                stack.extend(adjacent - visted)
+        return visted
+
+if __name__ == "__main__":
+
+    g = Graph()
+    g.add_vertex('a')
+    g.add_vertex('b')
+    g.add_vertex('c')
+    g.add_vertex('d')
+    g.add_vertex('e')
+    g.add_vertex('f')
+
+    g.add_edge('a', 'b', 0)
+    g.add_edge('a', 'c', 0)
+    g.add_edge('b', 'd', 0)
+    g.add_edge('b', 'e', 0)
+    g.add_edge('e', 'f', 0)
+    g.add_edge('f', 'c', 0)
+
+    for v in g:
+        for w in v.get_connections():
+            vid = v.get_id()
+            wid = w.get_id()
+            print(f"({vid}, {wid}, {v.get_weight(w)})")
+    
+    for v in g:
+        print(f"g.vert_dict[{v.get_id()}] = {g.vert_dict[v.get_id()]}")
+    
+    traversal = g.dfs('a')
+    print(traversal)
